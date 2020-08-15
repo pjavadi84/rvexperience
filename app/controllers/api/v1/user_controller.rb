f9498a9c6afef4769c94bd62056c618cc79803c8
github username <github email address>
2020-08-14 21:05:26 -0700

    init cors, created nested attributes for user and its associations, and updated user controller

---
+++ app/controllers/api/v1/user_controller.rb
@@ -0,0 +1,40 @@
+class Api::v1::UserController < ApplicationController
+    def index 
+        @users = User.all
+        render json: @users, status: 200
+    end
+
+    def show
+        @user = User.find(params[:id])
+        render json: @user, status: 200
+    end
+    
+    def new
+        @user = User.new
+    end
+
+    def create 
+        @user = User.new(user_params)
+        if @user.save
+            flash[:success] = "successfully created a user"
+            render json: @user, status: 200
+        else
+            render json: @users, status: 400
+        end
+    end
+
+    def update
+        @user = User.find(params[:id])
+        @user.update(user_params)
+
+    end
+
+    def destroy 
+
+    end
+
+    private 
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :age, :number_of_guests, :address, :city, :state, :country, :zipcode, :email, :phone_number)
+    end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
+    validates :first_name, :last_name, :age, :number_of_guests, :address, :city, :state, :country, :zipcode, :email, :phone_number, presence: true
+
     has_many :reservations
     has_many :rvs, through: :reservations
     has_many :destinations
     has_many :likes
+
+    accepts_nested_attributes_for :reservations, :rvs, :destination, :likes, allow_destroy: true
 end
